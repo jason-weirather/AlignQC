e4f7fe2bd170573f95e4f2787d70513b02b94f1f
Jason L Weirather <jason.weirather@gmail.com>
2016-10-17 16:54:16 -0500

    add additional analysis scripts

---
+++ scripts/alignqcs_to_read_lengths.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+import argparse, sys, os
+from shutil import rmtree
+from multiprocessing import cpu_count
+from tempfile import mkdtemp, gettempdir
+from subprocess import Popen, PIPE
+
+def main():
+  #do our inputs
+  args = do_inputs()
+
+  for name in args.inputs:
+    cmd = 'alignqc dump '+name+' -e lengths.txt'
+    p = Popen(cmd.split(),stdout=PIPE)
+    lengths = []
+    for line in p.stdout:
+      f = line.rstrip().split("\t")
+      l = int(f[4])
+      lengths.append(l)
+    print name +"\t"+"\t".join([str(x) for x in sorted(lengths)])
+    p.communicate()
+
+  # Temporary working directory step 3 of 3 - Cleanup
+  if not args.specific_tempdir:
+    rmtree(args.tempdir)
+
+def do_inputs():
+  # Setup command line inputs
+  parser=argparse.ArgumentParser(description="Convert a list of xhtmls from alignqc into a list of read lengths for each file",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument('inputs',nargs='+',help="INPUT xhtml FILEs")
+  parser.add_argument('-o','--output',help="OUTPUTFILE or STDOUT if not set")
+  parser.add_argument('--threads',type=int,default=cpu_count(),help="INT number of threads to run. Default is system cpu count")
+  # Temporary working directory step 1 of 3 - Definition
+  group = parser.add_mutually_exclusive_group()
+  group.add_argument('--tempdir',default=gettempdir(),help="The temporary directory is made and destroyed here.")
+  group.add_argument('--specific_tempdir',help="This temporary directory will be used, but will remain after executing.")
+  args = parser.parse_args()
+  # Temporary working directory step 2 of 3 - Creation
+  setup_tempdir(args)
+  return args
+
+def setup_tempdir(args):
+  if args.specific_tempdir:
+    if not os.path.exists(args.specific_tempdir):
+      os.makedirs(args.specific_tempdir.rstrip('/'))
+    args.tempdir = args.specific_tempdir.rstrip('/')
+    if not os.path.exists(args.specific_tempdir.rstrip('/')):
+      sys.stderr.write("ERROR: Problem creating temporary directory\n")
+      sys.exit()
+  else:
+    args.tempdir = mkdtemp(prefix="weirathe.",dir=args.tempdir.rstrip('/'))
+    if not os.path.exists(args.tempdir.rstrip('/')):
+      sys.stderr.write("ERROR: Problem creating temporary directory\n")
+      sys.exit()
+  if not os.path.exists(args.tempdir):
+    sys.stderr.write("ERROR: Problem creating temporary directory\n")
+    sys.exit()
+  return 
+
+if __name__=="__main__":
+  main()
---
+++ scripts/alignqcs_to_read_type_count_per_cell_statistics.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+import sys, argparse, re, os
+from subprocess import Popen, PIPE
+from Bio.Statistics import average, median, standard_deviation
+
+def main():
+  parser = argparse.ArgumentParser(description="Assume standard pacbio ccs and subread read name formats, and ONT name formats where _pass_2D _pass_tem _pass_com _fail_2D _fail_tem or _fail_com have been appended to the name.",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument('inputs',nargs='+',help="Specify xhtml files")
+  parser.add_argument('--aligned',action='store_true',help="restrict output to aligned reads only")
+  args = parser.parse_args()
+  
+  p_pbccs = re.compile('^m[^\/]+\/\d+\/ccs$')
+  p_pbsub = re.compile('^m[^\/]+\/\d+\/\d+_\d+$')
+  p_ontpass2D = re.compile('^\S+_pass_2D$')
+  p_ontpasstem = re.compile('^\S+_pass_tem$')
+  p_ontpasscom = re.compile('^\S+_pass_com$')
+  p_ontfail2D = re.compile('^\S+_fail_2D$')
+  p_ontfailtem = re.compile('^\S+_fail_tem$')
+  p_ontfailcom = re.compile('^\S+_fail_com$')
+  results = []
+  for fname in args.inputs:
+    sys.stderr.write("processing "+fname+"\n")
+    c = {
+    'pacbio':0,
+    'ont':0,
+    'ccs':0,
+    'sub':0,
+    'ontpass2D':0,
+    'ontpasstem':0,
+    'ontpasscom':0,
+    'ontfail2D':0,
+    'ontfailtem':0,
+    'ontfailcom':0,
+    'ontpass':0,
+    'ontfail':0,
+    'ont2D':0,
+    'ont1D':0,
+    'other':0 }
+    cmd = 'alignqc dump '+fname+' -e lengths.txt'
+    p = Popen(cmd.split(),stdout=PIPE)
+    for line in p.stdout:
+      f = line.rstrip().split("\t")
+      bp = int(f[3])
+      if args.aligned and bp == 0: continue
+      rname = f[0]
+      if p_pbccs.match(rname):
+        c['ccs']+=1
+        c['pacbio']+=1
+      elif p_pbsub.match(rname):
+        c['sub'] += 1
+        c['pacbio']+=1
+      elif p_ontpass2D.match(rname):
+        c['ontpass2D'] += 1
+        c['ont2D'] += 1
+        c['ont']+=1
+        c['ontpass']+=1
+      elif p_ontpasstem.match(rname):
+        c['ontpasstem'] += 1
+        c['ont1D'] += 1
+        c['ont']+=1
+        c['ontpass']+=1
+      elif p_ontpasscom.match(rname):
+        c['ontpasscom'] += 1
+        c['ont1D'] += 1
+        c['ont']+=1
+        c['ontpass']+=1
+      elif p_ontfail2D.match(rname):
+        c['ontfail2D'] += 1
+        c['ont2D'] += 1
+        c['ont']+=1
+        c['ontfail']+=1
+      elif p_ontfailtem.match(rname):
+        c['ontfailtem'] += 1
+        c['ont1D'] += 1
+        c['ont']+=1
+        c['ontfail']+=1
+      elif p_ontfailcom.match(rname):
+        c['ontfailcom'] += 1
+        c['ont1D'] += 1
+        c['ont']+=1
+        c['ontfail']+=1
+      else:
+        c['other']+=1
+    p.communicate()
+    results.append(c)
+  k = results[0].keys()
+  print "feature\tsum\tmedian\taverage\tstandard deviation"
+  for feature in sorted(k):
+    arr = [x[feature] for x in results]
+    print feature+"\t"+str(sum(arr))+"\t"+str(median(arr))+"\t"+str(average(arr))+"\t"+str(standard_deviation(arr))
+
+if __name__=="__main__":
+  main()
