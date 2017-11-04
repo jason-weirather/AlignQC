4924ee1a5515ef43f8455d79900ba75db2f75979
Jason L Weirather <jason-weirather@users.noreply.github.com>
2017-11-04 17:29:21 -0500

    update version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_folder,'README.md'),encoding='utf-8') as inf:
 
 setup(
   name='AlignQC',
-  version='2.0.4',
+  version='2.0.5',
   description='Python and R based tool for assessing the quality of alignments',
   long_description=long_description,
   url='https://github.com/jason-weirather/AlignQC',
@@ -29,7 +29,7 @@ setup(
   test_suite='tests',
   packages=['alignqc'],
   package_data={'alignqc':['data/mystyle.css','*.r']},
-  install_requires = ['seq-tools==1.0.7'],
+  install_requires = ['seq-tools==1.0.10'],
   entry_points = {
     'console_scripts':['alignqc=alignqc.alignqc:entry_point']
   }
